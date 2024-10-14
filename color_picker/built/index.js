3997cc2eb655f639a357a5996985c63a27dd47e4
DGMC117 <dgarciadm@gmail.com>
2024-10-14 20:56:29 +0200

    feat color_picker: finished color picker.

--- color_picker/built/index.js
+++ color_picker/built/index.js
@@ -1,1 +1,8 @@
 "use strict";
+let buttonCollection = document.getElementsByTagName("button");
+for (let i = 0; i < buttonCollection.length; i++) {
+    buttonCollection[i].addEventListener("click", () => {
+        let targetDiv = document.getElementById("targetDiv");
+        targetDiv.style.backgroundColor = buttonCollection[i].value;
+    });
+}
---
+++ color_picker/src/ts/index.ts
@@ -0,0 +1,9 @@
+let buttonCollection: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");
+
+for (let i = 0; i < buttonCollection.length; i++) {
+    buttonCollection[i].addEventListener("click", () => {
+        let targetDiv: HTMLElement = document.getElementById("targetDiv")!;
+
+        targetDiv.style.backgroundColor = buttonCollection[i].value;
+    });
+}
